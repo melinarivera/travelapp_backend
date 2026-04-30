a85dbc789a92d606bb44ca10385df1ccfa13c6d5
Melina <melina.rivera@gmail.com>
2026-04-30 19:01:53 +0200

    feat: ruta protegida /me con middleware de sesión

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,15 @@
 import express from 'express'
 import { login } from '../controllers/authController.js'
+import { verifySession } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.post('/login', login)
+router.get('/me', verifySession, (req, res) => {
+  res.status(200).json({
+    message: 'Sesión válida',
+    user: req.user
+  })
+})
 
 export default router
